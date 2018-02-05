b7917d3f56edc95818870c01af291eb0710a5941
Ravindra Khandagale <ravindrakk143@gmail.com>
2018-02-05 11:52:38 +0530

    initial commit

---
+++ React/first-react-project/webpack.config.js
@@ -0,0 +1,20 @@
+module.export = {
+    entry: './entry.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module:{
+        loaders: [
+            { test: /\.css$/loader: 'style!css'},
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel',
+                query:{
+                    presets: ['es2015','stage-0','react'],
+                    plugins: ['transfer-runtime']
+                }
+            }
+        ]
+    }
+}; 
