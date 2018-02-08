1c4bbaac6ff2cec04b59d4ac2e7912ee11c37b30
Ravindra Khandagale <ravindrakk143@gmail.com>
2018-02-08 15:41:53 +0530

    initial commit

--- React/first-react-project/webpack.config.js
+++ React/first-react-project/webpack.config.js
@@ -6,9 +6,11 @@ module.export = {
     },
     module:{
         loaders: [
-            { test: /\.css$/loader: 'style!css'},
+            { test: /\.css$/, loader: 'style!css'},
+            
             {
                 test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
                 loader: 'babel',
                 query:{
                     presets: ['es2015','stage-0','react'],
